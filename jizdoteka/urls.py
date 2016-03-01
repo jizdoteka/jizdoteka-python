4aaa039f3a3e650a1cd0182c6d2a504134a8e8ba
Kosieh Barter <kaja.valek@gmail.com>
2016-03-01 22:04:59 +0100

    Opraveno pro splneni PEP8
    Signed-off-by: Kosieh Barter <kaja.valek@gmail.com>

--- jizdoteka/urls.py
+++ jizdoteka/urls.py
@@ -36,12 +36,12 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     ## Nutne pro prihlaseni
-    url(r'^login_screen/$', views.LoginScreen.as_view(), name = "login_screen"),
-    url(r'^logout_user/$', views.logout_user, name = "logout_user"),
-    url(r'^register/$', views.RegisterScreen.as_view(), name = "register"),
-    url(r'^user/$', views.UserManagement.as_view(), name = "user"),
+    url(r'^login_screen/$', views.LoginScreen.as_view(), name="login_screen"),
+    url(r'^logout_user/$', views.logout_user, name="logout_user"),
+    url(r'^register/$', views.RegisterScreen.as_view(), name="register"),
+    url(r'^user/$', views.UserManagement.as_view(), name="user"),
 
-    url(r'^cars/$', views.CarManagement.as_view(), name = "cars"),
+    url(r'^cars/$', views.CarManagement.as_view(), name="cars"),
 
     #url(r'^user_mgmt/$', views.user_mgmt, name = "user_mgmt"),
     url(r'^accounts/', include('allauth.urls')),
