mpavlase/jizdoteka-web

Integrated Google maps API - searching, also among waypoints
There is also POC page for creating new journeys: server/new

jizdoteka/urls.py
@@ -20,5 +20,6 @@ from apps.web import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.JourneyList.as_view(), name='home'),
+    url(r'^new/$', views.JourneyCreate.as_view(), name='journey_new'),
     url(r'^(?P<pk>[0-9]+)$', views.JourneyDetail.as_view(), name='journey_detail'),
 ]

jizdoteka/jizdoteka-python

jizdoteka/urls.py
@@ -20,6 +20,7 @@ from apps.web.views import journey
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', journey.JourneyList.as_view(), name='home'),
+    url(r'^new/$', journey.JourneyCreate.as_view(), name='journey_new'),
     url(r'^(?P<pk>[0-9]+)$',
         journey.JourneyDetail.as_view(),
         name='journey_detail'